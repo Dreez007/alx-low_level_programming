df0ad79c2ed9dfd6e25550b4443223f4b8387671
Idris Ghanni <idrisghanni@gmail.com>
2022-09-08 15:43:25 -0700

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/** 
+ * Main is the entry point
+ *
+ * Return is always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
